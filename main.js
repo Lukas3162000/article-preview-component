93dc6fe40afd9247dc118d45ad8c3cd9a51c3df1
Lukas3162000 <Lukis3162000@aol.com>
2024-12-05 15:51:52 +0100

    JS fortschritt - onClick Logik und show wie hide-Funktion separiert. Fehlend: der Mobile-Active State

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 const shareBtn = document.querySelector(".share-btn");
-let isHovered = false;
-
+const shareBubble = document.querySelector(".share-bubble");
+const shareContainer = document.querySelector(".share-container");
 const shareIcon = document.querySelector('.share-icon');
 
-shareBtn.addEventListener('mouseover', () => {
-    isHovered = true;
-    console.log('Hover-Zustand:', isHovered);
+let isShown = false;
 
+function showBubble(){
+    shareBubble.style.visibility = "visible";
+    shareBubble.style.opacity = "1";
+};
+
+function hideBubble(){
+    shareBubble.style.visibility = "hidden";
+    shareBubble.style.opacity = "0";
+};
+
+
+//Hoverstate
+shareBtn.addEventListener('mouseover', () => {
     shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
     shareIcon.style.fill = '#ffffff';
-
-    
 });
 
 shareBtn.addEventListener('mouseout', () =>{
-    isHovered = false;
-    console.log('Hover-Zustand:', isHovered);
-
     shareBtn.style.backgroundColor = 'hsl(210,46%,95%)';
     shareIcon.style.fill = '#6E8098';
 });
+
+shareBtn.addEventListener('click', () => {
+    if(!isShown){
+    showBubble();
+    isShown = true;
+    } else{
+    hideBubble();
+    isShown = false;
+    }
+});
+
+
