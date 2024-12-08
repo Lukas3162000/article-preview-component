a6cdd0837afa98c8a84ef8c3e3f473fb6e7f1817
Lukas3162000 <Lukis3162000@aol.com>
2024-12-08 16:43:31 +0100

    Almost done with JS logic for Share-div

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ const shareBubble = document.querySelector(".share-bubble");
 const shareContainer = document.querySelector(".share-container");
 const shareIcon = document.querySelector('.share-icon');
 
+const newShare = document.querySelector('.new-Share');
+const newIcon = document.querySelector('.new-Icon');
+const mobileShare = document.querySelector('.MobileShare');
+const mobileNormal = document.querySelector('.AuthorRow');
+
 let isShown = false;
 
+
+//Desktop Sharebubble settings
 function showBubble(){
     shareBubble.style.visibility = "visible";
     shareBubble.style.opacity = "1";
@@ -16,7 +23,21 @@ function hideBubble(){
 };
 
 
-//Hoverstate
+
+//Mobile Share  settings
+function showMobileShare(){
+    mobileShare.style.display = "flex";
+    mobileNormal.style.display = "none";
+};
+
+function hideMobileShare(){
+    mobileNormal.style.display = "flex";
+    mobileShare.style.display = "none";
+};
+
+
+
+//Desktop Hoverstate 
 shareBtn.addEventListener('mouseover', () => {
     shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
     shareIcon.style.fill = '#ffffff';
@@ -27,6 +48,9 @@ shareBtn.addEventListener('mouseout', () =>{
     shareIcon.style.fill = '#6E8098';
 });
 
+
+
+//Share-Bubble-Logic on Desktop
 shareBtn.addEventListener('click', () => {
     if(!isShown){
     showBubble();
@@ -38,3 +62,26 @@ shareBtn.addEventListener('click', () => {
 });
 
 
+
+//Mobile-Hoverstate
+newShare.addEventListener('mouseover', () => {
+    newShare.style.backgroundColor = 'hsl(210,46%,95%)';
+    newIcon.style.fill = 'hsl(214, 17%, 51%)';
+});
+
+newShare.addEventListener('mouseout', () =>{
+    newShare.style.backgroundColor = 'hsl(214, 17%, 51%)';
+    newIcon.style.fill = '#ffffff';
+});
+
+
+//Share-Div-Logic on mobile
+newShare.addEventListener('click', () => {
+    if(isShown){
+    showMobileShare();
+    isShown = true;
+    } else{
+    hideMobileShare();
+    isShown = false;
+    }
+});
